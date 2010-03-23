3681c542ac7a01ecf521ba44c9db927c4f47d15d
mitchell <70453897+orbitalquark@users.noreply.github.com>
2010-03-23 01:05:08 -0400

    Modified lexers/haskell.lua by Alex Suraci.

--- lexers/haskell.lua
+++ lexers/haskell.lua
@@ -1,5 +1,6 @@
 -- Copyright 2006-2010 Mitchell mitchell<att>caladbolg.net. See LICENSE.
 -- Haskell LPeg Lexer
+-- Modified by Alex Suraci
 
 module(..., package.seeall)
 local P, R, S = lpeg.P, lpeg.R, lpeg.S
@@ -12,9 +13,10 @@ local block_comment = '{-' * (any - '-}')^0 * P('-}')^-1
 local comment = token('comment', line_comment + block_comment)
 
 -- strings
-local sq_str = delimited_range("'", nil, true, false, '\n')
-local dq_str = delimited_range('"', '\\', true)
-local string = token('string', sq_str + dq_str)
+local string = token('string', delimited_range('"', '\\'))
+
+-- chars
+local char = token('char', delimited_range("'", "\\", false, false, '\n'))
 
 -- numbers
 local number = token('number', float + integer)
@@ -23,35 +25,28 @@ local number = token('number', float + integer)
 local keyword = token('keyword', word_match(word_list{
   'case', 'class', 'data', 'default', 'deriving', 'do', 'else', 'if', 'import',
   'in', 'infix', 'infixl', 'infixr', 'instance', 'let', 'module', 'newtype',
-  'of', 'then', 'type', 'where', '_', 'as', 'qualified', 'hiding',
-  'EQ', 'False', 'GT', 'Just', 'LT', 'Left', 'Nothing', 'Right', 'True',
-  -- operators
-  'quot', 'rem', 'div', 'mod', 'elem', 'notElem', 'seq'
+  'of', 'then', 'type', 'where', '_', 'as', 'qualified', 'hiding'
 }))
-
--- types
-local type = token('type', word_match(word_list{
-  'Addr', 'Bool', 'Bounded', 'Char', 'Double', 'Either', 'Enum', 'Eq',
-  'FilePath', 'Float', 'Floating', 'Fractional', 'Functor', 'IO', 'IOError',
-  'IOResult', 'Int', 'Integer', 'Integral', 'Ix', 'Maybe', 'Monad', 'Num',
-  'Ord', 'Ordering', 'Ratio', 'Rational', 'Read', 'ReadS', 'Real', 'RealFloat',
-  'RealFrac', 'Show', 'ShowS', 'String'
-}))
-
--- identifiers
-local word = (alpha + '_') * (alnum + S("._'#"))^0
-local identifier = token('identifier', word)
 
 -- operators
-local operator = token('operator', S('.&:<>+-*/%^=|@~!$()[]{}'))
+local op = punct - S('()[]{}')
+local operator = token('operator', op)
+
+-- identifiers
+local word = (alnum + S("._'#"))^0
+local identifier = token('identifier', (alpha + '_') * word)
+
+-- types & type constructors
+local constructor = token('type', (upper * word) + (P(":") * (op^1 - P(":"))))
 
 function LoadTokens()
   local haskell = haskell
   add_token(haskell, 'whitespace', ws)
   add_token(haskell, 'keyword', keyword)
-  add_token(haskell, 'type', type)
+  add_token(haskell, 'type', constructor)
   add_token(haskell, 'identifier', identifier)
   add_token(haskell, 'string', string)
+  add_token(haskell, 'char', char)
   add_token(haskell, 'comment', comment)
   add_token(haskell, 'number', number)
   add_token(haskell, 'operator', operator)
