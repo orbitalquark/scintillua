de4d3d77c3bdaf2997d5360278f72c5d32c27c41
mitchell <70453897+orbitalquark@users.noreply.github.com>
2010-08-11 23:05:45 -0400

    Create patches from new Scintilla hg repository.

--- gen_patch.sh
+++ gen_patch.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cd scintilla
+hg diff > a.patch
+sed -i -e 's/^\([+-]\{3\}\) [ab]/\1 scintilla/g;' a.patch
 cd ../
-lua scintillua/create_patch.lua
-mv scintillua.patch scintillua/scintillua.patch
+cd scite
+hg diff > a.patch
+sed -i -e 's/^\([+-]\{3\}\) [ab]/\1 scite/g;' a.patch
+cd ../
+cat scintilla/a.patch scite/a.patch > scintillua.patch
+rm scintilla/a.patch scite/a.patch
