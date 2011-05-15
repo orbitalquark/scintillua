8383e7bd787e82740281c7be6e6b54d3e06651f9
mitchell <70453897+orbitalquark@users.noreply.github.com>
2011-05-15 02:51:52 -0400

    Implemented syntactic code folding for Lua; lexers/lua.lua

--- lexers/lua.lua
+++ lexers/lua.lua
@@ -6,6 +6,7 @@
 local l = lexer
 local token, style, color, word_match = l.token, l.style, l.color, l.word_match
 local P, R, S = l.lpeg.P, l.lpeg.R, l.lpeg.S
+local ipairs = ipairs
 
 module(...)
 
@@ -28,7 +29,8 @@ local comment = token(l.COMMENT, block_comment + line_comment)
 -- strings
 local sq_str = l.delimited_range("'", '\\', true)
 local dq_str = l.delimited_range('"', '\\', true)
-local string = token(l.STRING, sq_str + dq_str + longstring)
+local string = token(l.STRING, sq_str + dq_str) +
+               token('longstring', longstring)
 
 -- numbers
 local lua_integer = P('-')^-1 * (l.hex_num + l.dec_num)
@@ -73,3 +75,56 @@ _rules = {
   { 'operator', operator },
   { 'any_char', l.any_char },
 }
+
+_tokenstyles = {
+  { 'longstring', l.style_string }
+}
+
+-- Folding.
+local l_get_style_at = l.get_style_at
+local fold_increments = {
+  keyword = {
+    ['if'] = 1, ['do'] = 1, ['function'] = 1, ['repeat'] = 1,
+    ['end'] = -1, ['until'] = -1
+  },
+  operator = { ['('] = 1, ['{'] = 1, [')'] = -1, ['}'] = -1 },
+  comment = { ['['] = 1, [']'] = -1 },
+  longstring = { ['['] = 1, [']'] = -1 }
+}
+
+function _fold(text, start_pos, start_line, start_level)
+  local folds = {}
+  if text == '' then return folds end
+  local i, prev_level = start_line, start_level
+  local current_level = prev_level
+  local lines = {}
+  for p, l in text:gmatch('()(.-)\r?\n') do lines[#lines + 1] = { p, l } end
+  lines[#lines + 1] = { text:match('()([^\r\n]*)$') }
+  for _, m in ipairs(lines) do
+    local pos, line = m[1], m[2]
+    if #line > 0 then
+      for s, word in line:gmatch('()(%l+)') do
+        local style = l_get_style_at(start_pos + pos + s - 1)
+        if fold_increments[style] then
+          current_level = current_level + (fold_increments[style][word] or 0)
+        end
+      end
+      for s, char in line:gmatch('()([%({%)}%[%]])') do
+        local style = l_get_style_at(start_pos + pos + s - 1)
+        if fold_increments[style] then
+          current_level = current_level + (fold_increments[style][char] or 0)
+        end
+      end
+      if current_level > prev_level then
+        folds[i] = { prev_level, l.SC_FOLDLEVELHEADERFLAG }
+      else
+        folds[i] = { prev_level }
+      end
+      prev_level = current_level
+    else
+      folds[i] = { prev_level, l.SC_FOLDLEVELWHITEFLAG }
+    end
+    i = i + 1
+  end
+  return folds
+end
