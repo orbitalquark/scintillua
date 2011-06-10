0f44e3f78712308b4cba5e4c62a36e3bc59cdb0d
mitchell <70453897+orbitalquark@users.noreply.github.com>
2011-06-10 17:13:51 -0400

    Added folding for Verilog; lexers/verilog.lua

--- lexers/verilog.lua
+++ lexers/verilog.lua
@@ -83,3 +83,17 @@ _rules = {
   { 'operator', operator },
   { 'any_char', l.any_char },
 }
+
+_foldsymbols = {
+  _patterns = { '[a-z]+', '[%(%){}]', '/%*', '%*/' },
+  comment = { ['/*'] = 1, ['*/'] = -1 },
+  keyword = {
+    case = 1, casex = 1, casez = 1, ['function'] = 1, fork = 1, table = 1,
+    task = 1, generate = 1, specify = 1, primitive = 1, ['module'] = 1,
+    begin = 1,
+    endcase = -1, endfunction = -1, join = -1, endtask = -1, endgenerate = -1,
+    endtable = -1, endspecify = -1, endprimitive = -1, endmodule = -1,
+    ['end'] = -1
+  },
+  operator = { ['('] = 1, [')'] = -1, ['{'] = 1, ['}'] = -1 }
+}
