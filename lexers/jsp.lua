e99fd5cd4c2e89f4a830f19274a41aedf6c85207
mitchell <70453897+orbitalquark@users.noreply.github.com>
2011-06-09 21:37:27 -0400

    Added folding for JSP; lexers/jsp.lua

--- lexers/jsp.lua
+++ lexers/jsp.lua
@@ -27,3 +27,8 @@ l.embed_lexer(html, java, java_start_rule, java_end_rule, true)
 _tokenstyles = {
   { 'jsp_tag', l.style_embedded },
 }
+
+local _foldsymbols = html._foldsymbols
+_foldsymbols._patterns[#_foldsymbols._patterns + 1] = '<%%'
+_foldsymbols._patterns[#_foldsymbols._patterns + 1] = '%%>'
+_foldsymbols.jsp_tag = { ['<%'] = 1, ['%>'] = -1 }
