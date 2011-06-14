0aa5fed7affce7ea46f595920b7624fe6a78215c
mitchell <70453897+orbitalquark@users.noreply.github.com>
2011-06-14 18:01:06 -0400

    Added Scala lexer.

---
+++ lexers/scala.lua
@@ -0,0 +1,74 @@
+-- Copyright 2006-2011 JMS. See LICENSE.
+-- Scala LPeg Lexer.
+
+local l = lexer
+local token, style, color, word_match = l.token, l.style, l.color, l.word_match
+local P, R, S = l.lpeg.P, l.lpeg.R, l.lpeg.S
+
+module(...)
+
+-- Whitespace.
+local ws = token(l.WHITESPACE, l.space^1)
+
+-- Comments.
+local line_comment = '//' * l.nonnewline_esc^0
+local block_comment = '/*' * (l.any - '*/')^0 * P('*/')^-1
+local comment = token(l.COMMENT, line_comment + block_comment)
+
+-- Strings.
+local sq_str = l.delimited_range("'", '\\', true, false, '\n')
+local dq_str = l.delimited_range('"', '\\', true, false, '\n')
+local tq_str = '"""' * (l.any - '"""')^0 * P('"""')^-1
+local string = token(l.STRING, tq_str + sq_str + dq_str)
+
+-- Numbers.
+local number = token(l.NUMBER, (l.float + l.integer) * S('LlFfDd')^-1)
+
+-- Keywords.
+local keyword = token(l.KEYWORD, word_match {
+  'abstract', 'case', 'catch', 'class', 'def', 'do', 'else', 'extends', 'false',
+  'final', 'finally', 'for', 'forSome', 'if', 'implicit', 'import', 'lazy',
+  'match', 'new', 'null', 'object', 'override', 'package', 'private',
+  'protected', 'return', 'sealed', 'super', 'this', 'throw', 'trait', 'try',
+  'true', 'type', 'val', 'var', 'while', 'with', 'yield'
+})
+
+-- Types.
+local type = token(l.TYPE, word_match {
+  'Array', 'Boolean', 'Buffer', 'Byte', 'Char', 'Collection', 'Double', 'Float',
+  'Int', 'Iterator', 'LinkedList', 'List', 'Long', 'Map', 'None', 'Option',
+  'Set', 'Short', 'SortedMap', 'SortedSet', 'String', 'TreeMap', 'TreeSet'
+})
+
+-- Identifiers.
+local identifier = token(l.IDENTIFIER, l.word)
+
+-- Operators.
+local operator = token(l.OPERATOR, S('+-/*%<>!=^&|?~:;.()[]{}'))
+
+-- Functions.
+local func = token(l.FUNCTION, l.word) * #P('(')
+
+-- Classes.
+local class_sequence = token(l.KEYWORD, P('class')) * ws^1 *
+                       token(l.CLASS, l.word)
+
+_rules = {
+  { 'whitespace', ws },
+  { 'class', class_sequence },
+  { 'keyword', keyword },
+  { 'type', type },
+  { 'function', func},
+  { 'identifier', identifier },
+  { 'string', string },
+  { 'comment', comment },
+  { 'number', number },
+  { 'operator', operator },
+  { 'any_char', l.any_char },
+}
+
+_foldsymbols = {
+  _patterns = { '[{}]', '/%*', '%*/' },
+  [l.OPERATOR] = { ['{'] = 1, ['}'] = -1 },
+  [l.COMMENT] = { ['/*'] = 1, ['*/'] = -1 }
+}
