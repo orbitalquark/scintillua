855a895d52622167f679137812c03220474781c6
mitchell <70453897+orbitalquark@users.noreply.github.com>
2011-06-30 21:19:01 -0400

    Updated LaTeX and TeX lexers from Robert Gieseke.

--- lexers/latex.lua
+++ lexers/latex.lua
@@ -1,6 +1,7 @@
 -- Copyright 2006-2011 Mitchell mitchell<att>caladbolg.net. See LICENSE.
 -- Latex LPeg lexer.
 -- Modified by Brian Schott.
+-- Modified by Robert Gieseke.
 
 local l = lexer
 local token, style, color, word_match = l.token, l.style, l.color, l.word_match
@@ -15,26 +16,48 @@ local ws = token(l.WHITESPACE, l.space^1)
 local line_comment = '%' * l.nonnewline^0
 local block_comment = '\\begin{comment}' * (l.any - '\\end{comment}')^0 *
                       P('\\end{comment}')^-1
-local comment = token(l.COMMENT, block_comment + line_comment)
+local comment = token(l.COMMENT, line_comment + block_comment)
 
--- Strings.
-local math_string = '$$' * (l.any - '$$')^0 * '$$' +
-                    l.delimited_range('$', '\\', true, false, '\n')
-local string = token(l.STRING, math_string)
+-- Sections.
+local section_keywords = word_match {
+  'part', 'chapter', 'section', 'subsection', 'subsubsection', 'paragraph',
+  'subparagraph'
+}
+local parts = token('parts', '\\' * section_keywords * P('*')^-1)
+
+-- Math environments.
+local math_word = word_match {
+  'align', 'displaymath', 'eqnarray', 'equation', 'gather', 'math', 'multline'
+}
+local math_begin_end = (P('begin') + P('end')) *
+                       '{' * math_word * P('*')^-1 * '}'
+local math = token('math', '$' + '\\' * (S('[]()') + math_begin_end))
+
+-- LaTeX environments.
+local environment = token('environment', '\\' * (P('begin') + P('end')) *
+                          '{' * l.word * P('*')^-1 * '}')
 
 -- Commands.
-local command = token(l.KEYWORD, '\\' * l.word)
+local command = token(l.KEYWORD, '\\' * (l.alpha^1 + S('#$&~_^%{}')))
 
 -- Operators.
-local operator = token(l.OPERATOR, S('$&%#{}'))
+local operator = token(l.OPERATOR, S('&#{}[]'))
 
 _rules = {
   { 'whitespace', ws },
   { 'comment', comment },
-  { 'string', string },
+  { 'math', math },
+  { 'environment', environment },
+  { 'parts', parts},
   { 'keyword', command },
   { 'operator', operator },
   { 'any_char', l.any_char },
+}
+
+_tokenstyles = {
+  { 'environment', l.style_tag },
+  { 'math', l.style_function },
+  { 'parts', l.style_class },
 }
 
 _foldsymbols = {
@@ -42,6 +65,6 @@ _foldsymbols = {
   [l.COMMENT] = {
     ['\\begin'] = 1, ['\\end'] = -1, ['%'] = l.fold_line_comments('%')
   },
-  [l.KEYWORD] = { ['\\begin'] = 1, ['\\end'] = -1 },
+  ['environment'] = { ['\\begin'] = 1, ['\\end'] = -1 },
   [l.OPERATOR] = { ['{'] = 1, ['}'] = -1 }
 }
--- lexers/tex.lua
+++ lexers/tex.lua
@@ -1,11 +1,6 @@
 -- Copyright 2006-2011 Mitchell mitchell<att>caladbolg.net. See LICENSE.
--- April 2010 Robert Gieseke, combined LaTeX and ConTeXt lexing.
--- TeX LPeg lexer.
-
--- TODO: Embed LuaTeX
---   ConTeXt: \directlua{...}
---     or \startlua ... \stoplua
---   LaTeX: see pdf 'LuaTEXtra references'
+-- Plain TeX LPeg lexer.
+-- Modified by Robert Gieseke.
 
 local l = lexer
 local token, style, color, word_match = l.token, l.style, l.color, l.word_match
@@ -17,32 +12,16 @@ module(...)
 local ws = token(l.WHITESPACE, l.space^1)
 
 -- Comments.
-local line_comment = '%' * l.nonnewline^0
-local block_comment = '\\begin{comment}' * (l.any - '\\end{comment}')^0 *
-                      P('\\end{comment}')^-1
-local comment = token(l.COMMENT, line_comment + block_comment)
+local comment = token(l.COMMENT, '%' * l.nonnewline^0)
 
--- Environments.
--- LaTeX environments.
-local env_latex = '\\' * (P('begin') + 'end') * '{' *  word_match({
-  'abstract', 'array', 'center', 'description', 'displaymath', 'document',
-  'enumerate', 'eqnarray', 'equation', 'figure', 'flushleft', 'flushright',
-  'itemize', 'list', 'math', 'minipage', 'picture', 'quotation', 'quote',
-  'tabbing', 'table', 'tabular', 'thebibliography', 'theorem', 'titlepage',
-  'trivlist', 'verbatim', 'verse'
-}) * '}'
-local env_latex_math = '\\' * S('[]()') + '$' -- covers '$$' as well
--- ConTeXt environments.
-local env_context = '\\' * (P('start') * l.word + 'stop' * l.word)
-local environment = token('environment', env_latex + env_latex_math +
-                          env_context)
+-- TeX environments.
+local environment = token('environment', '\\' * (P('begin') + 'end') * l.word)
 
 -- Commands.
-local escapes = S('$%_{}&#')
-local command = token(l.KEYWORD, '\\' * (l.alpha^1 + escapes))
+local command = token(l.KEYWORD, '\\' * (l.alpha^1 + S('#$&~_^%{}')))
 
 -- Operators.
-local operator = token(l.OPERATOR, S('$&%#{}[]'))
+local operator = token(l.OPERATOR, S('$&#{}[]'))
 
 _rules = {
   { 'whitespace', ws },
@@ -58,10 +37,8 @@ _tokenstyles = {
 }
 
 _foldsymbols = {
-  _patterns = { '\\[a-z]+', '[{}]', '%%' },
-  [l.COMMENT] = {
-    ['\\begin'] = 1, ['\\end'] = -1, ['%'] = l.fold_line_comments('%')
-  },
-  [l.KEYWORD] = { ['\\begin'] = 1, ['\\end'] = -1 },
+  _patterns = { '\\begin', '\\end', '[{}]', '%%' },
+  [l.COMMENT] = { ['%'] = l.fold_line_comments('%') },
+  ['environment'] = { ['\\begin'] = 1, ['\\end'] = -1 },
   [l.OPERATOR] = { ['{'] = 1, ['}'] = -1 }
 }
