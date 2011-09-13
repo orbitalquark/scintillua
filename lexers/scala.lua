bfcb686f491524502dbbbe46358f0fede3adc48c
mitchell <70453897+orbitalquark@users.noreply.github.com>
2011-09-12 21:19:34 -0400

    Recognize Scala symbols; lexers/scala.lua

--- lexers/scala.lua
+++ lexers/scala.lua
@@ -16,10 +16,10 @@ local block_comment = '/*' * (l.any - '*/')^0 * P('*/')^-1
 local comment = token(l.COMMENT, line_comment + block_comment)
 
 -- Strings.
-local sq_str = l.delimited_range("'", '\\', true, false, '\n')
+local symbol = "'" * l.word
 local dq_str = l.delimited_range('"', '\\', true, false, '\n')
 local tq_str = '"""' * (l.any - '"""')^0 * P('"""')^-1
-local string = token(l.STRING, tq_str + sq_str + dq_str)
+local string = token(l.STRING, tq_str + symbol + dq_str)
 
 -- Numbers.
 local number = token(l.NUMBER, (l.float + l.integer) * S('LlFfDd')^-1)
