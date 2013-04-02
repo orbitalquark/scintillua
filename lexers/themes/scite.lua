c6748776cbbde141a486b5771cef364bbf6c7aca
mitchell <70453897+orbitalquark@users.noreply.github.com>
2013-04-02 00:21:12 -0400

    No need for '!' in front of font faces in GTK anymore.

--- lexers/themes/scite.lua
+++ lexers/themes/scite.lua
@@ -37,12 +37,12 @@ l.style_regex       = l.style_string
 l.style_identifier  = l.style_nothing
 
 -- Default styles.
-local font_face = '!Monospace'
+local font_face = 'Monospace'
 local font_size = 11
 if WIN32 then
-  font_face = not GTK and 'Courier New' or '!Courier New'
+  font_face = 'Courier New'
 elseif OSX then
-  font_face = '!Monaco'
+  font_face = 'Monaco'
   font_size = 12
 end
 l.style_default = style{
