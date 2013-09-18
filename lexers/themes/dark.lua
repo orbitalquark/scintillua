b331f7b19e3ebc7377d932ca9136cf6c0f109aaf
mitchell <70453897+orbitalquark@users.noreply.github.com>
2013-09-17 22:29:06 -0400

    Updated themes to use new lexer property tables.

--- lexers/themes/dark.lua
+++ lexers/themes/dark.lua
@@ -2,53 +2,53 @@
 -- Dark lexer theme for Scintillua.
 -- Contributions by Ana Balan.
 
-local set_property = lexer.set_property
+local property = lexer.property
 
 -- Greyscale colors.
---set_property('color.dark_black', '#000000')
-set_property('color.black', '#1A1A1A')
-set_property('color.light_black', '#333333')
---set_property('color.grey_black', '#4D4D4D')
-set_property('color.dark_grey', '#666666')
---set_property('color.grey', '#808080')
-set_property('color.light_grey', '#999999')
---set_property('color.grey_white', '#B3B3B3')
-set_property('color.dark_white', '#CCCCCC')
---set_property('color.white', '#E6E6E6')
---set_property('color.light_white', '#FFFFFF')
+--property['color.dark_black'] = '#000000'
+property['color.black'] = '#1A1A1A'
+property['color.light_black'] = '#333333'
+--property['color.grey_black'] = '#4D4D4D'
+property['color.dark_grey'] = '#666666'
+--property['color.grey'] = '#808080'
+property['color.light_grey'] = '#999999'
+--property['color.grey_white'] = '#B3B3B3'
+property['color.dark_white'] = '#CCCCCC'
+--property['color.white'] = '#E6E6E6'
+--property['color.light_white'] = '#FFFFFF'
 
 -- Dark colors.
---set_property('color.dark_red', '#661A1A')
---set_property('color.dark_yellow', '#66661A')
---set_property('color.dark_green', '#1A661A')
---set_property('color.dark_teal', '#1A6666')
---set_property('color.dark_purple', '#661A66')
---set_property('color.dark_orange', '#B3661A')
---set_property('color.dark_pink', '#B36666')
---set_property('color.dark_lavender', '#6666B3')
---set_property('color.dark_blue', '#1A66B3')
+--property['color.dark_red'] = '#661A1A'
+--property['color.dark_yellow'] = '#66661A'
+--property['color.dark_green'] = '#1A661A'
+--property['color.dark_teal'] = '#1A6666'
+--property['color.dark_purple'] = '#661A66'
+--property['color.dark_orange'] = '#B3661A'
+--property['color.dark_pink'] = '#B36666'
+--property['color.dark_lavender'] = '#6666B3'
+--property['color.dark_blue'] = '#1A66B3'
 
 -- Normal colors.
-set_property('color.red', '#994D4D')
-set_property('color.yellow', '#99994D')
-set_property('color.green', '#4D994D')
-set_property('color.teal', '#4D9999')
-set_property('color.purple', '#994D99')
-set_property('color.orange', '#E6994D')
---set_property('color.pink', '#E69999')
-set_property('color.lavender', '#9999E6')
-set_property('color.blue', '#4D99E6')
+property['color.red'] = '#994D4D'
+property['color.yellow'] = '#99994D'
+property['color.green'] = '#4D994D'
+property['color.teal'] = '#4D9999'
+property['color.purple'] = '#994D99'
+property['color.orange'] = '#E6994D'
+--property['color.pink'] = '#E69999'
+property['color.lavender'] = '#9999E6'
+property['color.blue'] = '#4D99E6'
 
 -- Light colors.
-set_property('color.light_red', '#CC8080')
-set_property('color.light_yellow', '#CCCC80')
-set_property('color.light_green', '#80CC80')
---set_property('color.light_teal', '#80CCCC')
---set_property('color.light_purple', '#CC80CC')
---set_property('color.light_orange', '#FFCC80')
---set_property('color.light_pink', '#FFCCCC')
---set_property('color.light_lavender', '#CCCCFF')
-set_property('color.light_blue', '#80CCFF')
+property['color.light_red'] = '#CC8080'
+property['color.light_yellow'] = '#CCCC80'
+property['color.light_green'] = '#80CC80'
+--property['color.light_teal'] = '#80CCCC'
+--property['color.light_purple'] = '#CC80CC'
+--property['color.light_orange'] = '#FFCC80'
+--property['color.light_pink'] = '#FFCCCC'
+--property['color.light_lavender'] = '#CCCCFF'
+property['color.light_blue'] = '#80CCFF'
 
 -- Default style.
 local font, size = 'Bitstream Vera Sans Mono', 10
@@ -57,37 +57,37 @@ if WIN32 then
 elseif OSX then
   font, size = 'Monaco', 12
 end
-set_property('style.default', 'font:'..font..',size:'..size..
-                              ',fore:$(color.light_grey),back:$(color.black)')
+property['style.default'] = 'font:'..font..',size:'..size..
+                            ',fore:$(color.light_grey),back:$(color.black)'
 
 -- Token styles.
-set_property('style.nothing', '')
-set_property('style.class', 'fore:$(color.light_yellow)')
-set_property('style.comment', 'fore:$(color.dark_grey)')
-set_property('style.constant', 'fore:$(color.red)')
-set_property('style.definition', 'fore:$(color.light_yellow)')
-set_property('style.error', 'fore:$(color.red),italics')
-set_property('style.function', 'fore:$(color.blue)')
-set_property('style.keyword', 'fore:$(color.dark_white)')
-set_property('style.label', 'fore:$(color.orange)')
-set_property('style.number', 'fore:$(color.teal)')
-set_property('style.operator', 'fore:$(color.yellow)')
-set_property('style.regex', 'fore:$(color.light_green)')
-set_property('style.string', 'fore:$(color.green)')
-set_property('style.preproc', 'fore:$(color.purple)')
-set_property('style.tag', 'fore:$(color.dark_white)')
-set_property('style.type', 'fore:$(color.lavender)')
-set_property('style.variable', 'fore:$(color.light_blue)')
-set_property('style.whitespace', '')
-set_property('style.embedded', '$(style.tag),back:$(color.light_black)')
-set_property('style.identifier', '$(style.nothing)')
+property['style.nothing'] = ''
+property['style.class'] = 'fore:$(color.light_yellow)'
+property['style.comment'] = 'fore:$(color.dark_grey)'
+property['style.constant'] = 'fore:$(color.red)'
+property['style.definition'] = 'fore:$(color.light_yellow)'
+property['style.error'] = 'fore:$(color.red),italics'
+property['style.function'] = 'fore:$(color.blue)'
+property['style.keyword'] = 'fore:$(color.dark_white)'
+property['style.label'] = 'fore:$(color.orange)'
+property['style.number'] = 'fore:$(color.teal)'
+property['style.operator'] = 'fore:$(color.yellow)'
+property['style.regex'] = 'fore:$(color.light_green)'
+property['style.string'] = 'fore:$(color.green)'
+property['style.preproc'] = 'fore:$(color.purple)'
+property['style.tag'] = 'fore:$(color.dark_white)'
+property['style.type'] = 'fore:$(color.lavender)'
+property['style.variable'] = 'fore:$(color.light_blue)'
+property['style.whitespace'] = ''
+property['style.embedded'] = '$(style.tag),back:$(color.light_black)'
+property['style.identifier'] = '$(style.nothing)'
 
 -- Predefined styles.
-set_property('style.linenumber', 'fore:$(color.dark_grey),back:$(color.black)')
-set_property('style.bracelight', 'fore:$(color.light_blue)')
-set_property('style.bracebad', 'fore:$(color.light_red)')
-set_property('style.controlchar', '$(style.nothing)')
-set_property('style.indentguide',
-             'fore:$(color.light_black),back:$(color.light_black)')
-set_property('style.calltip',
-             'fore:$(color.light_grey),back:$(color.light_black)')
+property['style.linenumber'] = 'fore:$(color.dark_grey),back:$(color.black)'
+property['style.bracelight'] = 'fore:$(color.light_blue)'
+property['style.bracebad'] = 'fore:$(color.light_red)'
+property['style.controlchar'] = '$(style.nothing)'
+property['style.indentguide'] = 'fore:$(color.light_black),'..
+                                'back:$(color.light_black)'
+property['style.calltip'] = 'fore:$(color.light_grey),'..
+                            'back:$(color.light_black)'
--- lexers/themes/scite.lua
+++ lexers/themes/scite.lua
@@ -1,18 +1,18 @@
 -- Copyright 2006-2013 Mitchell mitchell.att.foicica.com. See LICENSE.
 -- SciTE lexer theme for Scintillua.
 
-local set_property = lexer.set_property
+local property = lexer.property
 
-set_property('color.red', '#7F0000')
-set_property('color.yellow', '#7F7F00')
-set_property('color.green', '#007F00')
-set_property('color.teal', '#007F7F')
-set_property('color.purple', '#7F007F')
-set_property('color.orange', '#B07F00')
-set_property('color.blue', '#00007F')
-set_property('color.black', '#000000')
-set_property('color.grey', '#808080')
-set_property('color.white', '#FFFFFF')
+property['color.red'] = '#7F0000'
+property['color.yellow'] = '#7F7F00'
+property['color.green'] = '#007F00'
+property['color.teal'] = '#007F7F'
+property['color.purple'] = '#7F007F'
+property['color.orange'] = '#B07F00'
+property['color.blue'] = '#00007F'
+property['color.black'] = '#000000'
+property['color.grey'] = '#808080'
+property['color.white'] = '#FFFFFF'
 
 -- Default style.
 local font, size = 'Monospace', 11
@@ -21,35 +21,35 @@ if WIN32 then
 elseif OSX then
   font, size = 'Monaco', 12
 end
-set_property('style.default', 'font:'..font..',size:'..size..
-                              ',fore:$(color.black),back:$(color.white)')
+property['style.default'] = 'font:'..font..',size:'..size..
+                            ',fore:$(color.black),back:$(color.white)'
 
 -- Token styles.
-set_property('style.nothing', '')
-set_property('style.class', 'fore:$(color.black),bold')
-set_property('style.comment', 'fore:$(color.green)')
-set_property('style.constant', 'fore:$(color.teal),bold')
-set_property('style.definition', 'fore:$(color.black),bold')
-set_property('style.error', 'fore:$(color.red)')
-set_property('style.function', 'fore:$(color.black),bold')
-set_property('style.keyword', 'fore:$(color.blue),bold')
-set_property('style.label', 'fore:$(color.teal),bold')
-set_property('style.number', 'fore:$(color.teal)')
-set_property('style.operator', 'fore:$(color.black),bold')
-set_property('style.regex', '$(style.string)')
-set_property('style.string', 'fore:$(color.purple)')
-set_property('style.preproc', 'fore:$(color.yellow)')
-set_property('style.tag', 'fore:$(color.teal)')
-set_property('style.type', 'fore:$(color.blue)')
-set_property('style.variable', 'fore:$(color.black)')
-set_property('style.whitespace', '')
-set_property('style.embedded', 'fore:$(color.blue)')
-set_property('style.identifier', '$(style.nothing)')
+property['style.nothing'] = ''
+property['style.class'] = 'fore:$(color.black),bold'
+property['style.comment'] = 'fore:$(color.green)'
+property['style.constant'] = 'fore:$(color.teal),bold'
+property['style.definition'] = 'fore:$(color.black),bold'
+property['style.error'] = 'fore:$(color.red)'
+property['style.function'] = 'fore:$(color.black),bold'
+property['style.keyword'] = 'fore:$(color.blue),bold'
+property['style.label'] = 'fore:$(color.teal),bold'
+property['style.number'] = 'fore:$(color.teal)'
+property['style.operator'] = 'fore:$(color.black),bold'
+property['style.regex'] = '$(style.string)'
+property['style.string'] = 'fore:$(color.purple)'
+property['style.preproc'] = 'fore:$(color.yellow)'
+property['style.tag'] = 'fore:$(color.teal)'
+property['style.type'] = 'fore:$(color.blue)'
+property['style.variable'] = 'fore:$(color.black)'
+property['style.whitespace'] = ''
+property['style.embedded'] = 'fore:$(color.blue)'
+property['style.identifier'] = '$(style.nothing)'
 
 -- Predefined styles.
-set_property('style.linenumber', 'back:#C0C0C0')
-set_property('style.bracelight', 'fore:#0000FF,bold')
-set_property('style.bracebad', 'fore:#FF0000,bold')
-set_property('style.controlchar', '$(style.nothing)')
-set_property('style.indentguide', 'fore:#C0C0C0,back:$(color.white)')
-set_property('style.calltip', 'fore:$(color.white),back:#444444')
+property['style.linenumber'] = 'back:#C0C0C0'
+property['style.bracelight'] = 'fore:#0000FF,bold'
+property['style.bracebad'] = 'fore:#FF0000,bold'
+property['style.controlchar'] = '$(style.nothing)'
+property['style.indentguide'] = 'fore:#C0C0C0,back:$(color.white)'
+property['style.calltip'] = 'fore:$(color.white),back:#444444'
