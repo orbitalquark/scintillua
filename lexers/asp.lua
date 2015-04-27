88b116377c9801bae68a91069eba15b95e96d5ff
mitchell <70453897+orbitalquark@users.noreply.github.com>
2015-04-27 13:49:32 -0400

    ASP lexer improvements; lexers/asp.lua Thanks to Jeff Stone.

--- lexers/asp.lua
+++ lexers/asp.lua
@@ -19,11 +19,12 @@ l.embed_lexer(html, vb, vb_start_rule, vb_end_rule)
 -- Embedded VBScript.
 local vbs = l.load('vbscript')
 local script_element = word_match({'script'}, nil, html.case_insensitive_tags)
-local vbs_start_rule = #(P('<') * script_element * P(function(input, index)
-  if input:find('^%s+language%s*=%s*(["\'])vbscript%1', index) then
+local vbs_start_rule = #(P('<') * script_element * (P(function(input, index)
+  if input:find('^%s+language%s*=%s*(["\'])vbscript%1', index) or
+     input:find('^%s+type%s*=%s*(["\'])text/vbscript%1', index) then
     return index
   end
-end)) * html.embed_start_tag -- <script language="vbscript">
+end) + '>')) * html.embed_start_tag -- <script language="vbscript">
 local vbs_end_rule = #('</' * script_element * l.space^0 * '>') *
                      html.embed_end_tag -- </script>
 l.embed_lexer(html, vbs, vbs_start_rule, vbs_end_rule)
