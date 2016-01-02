3f1d833417e1b0608a37c3b347e39eac1ae6abf2
mitchell <70453897+orbitalquark@users.noreply.github.com>
2016-01-02 14:49:22 -0500

    Updated copyright date.

--- lexers/elixir.lua
+++ lexers/elixir.lua
@@ -1,4 +1,4 @@
--- Copyright 2015 Mitchell mitchell.att.foicica.com. See LICENSE.
+-- Copyright 2015-2016 Mitchell mitchell.att.foicica.com. See LICENSE.
 -- Contributed by Richard Philips.
 -- Elixer LPeg lexer.
 
