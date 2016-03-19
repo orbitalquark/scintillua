f661778bfa75267e218d3c337ccbf3d43162f5d5
mitchell <70453897+orbitalquark@users.noreply.github.com>
2016-03-18 22:58:46 -0400

    Updated JavaScript keywords; lexers/javascript.lua

--- lexers/javascript.lua
+++ lexers/javascript.lua
@@ -33,10 +33,10 @@ local keyword = token(l.KEYWORD, word_match{
   'const', 'continue', 'debugger', 'default', 'delete', 'do', 'double', 'else',
   'enum', 'export', 'extends', 'false', 'final', 'finally', 'float', 'for',
   'function', 'goto', 'if', 'implements', 'import', 'in', 'instanceof', 'int',
-  'interface', 'let', 'long', 'native', 'new', 'null', 'package', 'private',
-  'protected', 'public', 'return', 'short', 'static', 'super', 'switch',
-  'synchronized', 'this', 'throw', 'throws', 'transient', 'true', 'try',
-  'typeof', 'var', 'void', 'volatile', 'while', 'with', 'yield'
+  'interface', 'let', 'long', 'native', 'new', 'null', 'of', 'package',
+  'private', 'protected', 'public', 'return', 'short', 'static', 'super',
+  'switch', 'synchronized', 'this', 'throw', 'throws', 'transient', 'true',
+  'try', 'typeof', 'var', 'void', 'volatile', 'while', 'with', 'yield'
 })
 
 -- Identifiers.
