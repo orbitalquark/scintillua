d16a9df61daa197c9ffbf3d9a2e02bb4de994689
mitchell <70453897+orbitalquark@users.noreply.github.com>
2017-01-12 22:10:27 -0500

    Updated JavaScript lexer; lexers/javascript.lua Added new keywords.

--- lexers/javascript.lua
+++ lexers/javascript.lua
@@ -32,9 +32,9 @@ local keyword = token(l.KEYWORD, word_match{
   'abstract', 'boolean', 'break', 'byte', 'case', 'catch', 'char', 'class',
   'const', 'continue', 'debugger', 'default', 'delete', 'do', 'double', 'else',
   'enum', 'export', 'extends', 'false', 'final', 'finally', 'float', 'for',
-  'function', 'goto', 'if', 'implements', 'import', 'in', 'instanceof', 'int',
-  'interface', 'let', 'long', 'native', 'new', 'null', 'of', 'package',
-  'private', 'protected', 'public', 'return', 'short', 'static', 'super',
+  'function', 'get', 'goto', 'if', 'implements', 'import', 'in', 'instanceof',
+  'int', 'interface', 'let', 'long', 'native', 'new', 'null', 'of', 'package',
+  'private', 'protected', 'public', 'return', 'set', 'short', 'static', 'super',
   'switch', 'synchronized', 'this', 'throw', 'throws', 'transient', 'true',
   'try', 'typeof', 'var', 'void', 'volatile', 'while', 'with', 'yield'
 })
