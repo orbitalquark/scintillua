b2f776985da7d8790ba60d29d357e857f85d1701
mitchell <70453897+orbitalquark@users.noreply.github.com>
2017-12-14 11:15:28 -0500

    Make use of lexer option for case insensitive fold points.

--- lexers/apdl.lua
+++ lexers/apdl.lua
@@ -5,7 +5,7 @@ local lexer = require('lexer')
 local token, word_match = lexer.token, lexer.word_match
 local P, R, S = lpeg.P, lpeg.R, lpeg.S
 
-local lex = lexer.new('apdl')
+local lex = lexer.new('apdl', {case_insensitive_fold_points = true})
 
 -- Whitespace.
 lex:add_rule('whitespace', token(lexer.WHITESPACE, lexer.space^1))
@@ -67,11 +67,8 @@ lex:add_rule('operator', token(lexer.OPERATOR, S('+-*/$=,;()')))
 
 -- Fold points.
 lex:add_fold_point(lexer.KEYWORD, '*if', '*endif')
-lex:add_fold_point(lexer.KEYWORD, '*IF', '*ENDIF')
 lex:add_fold_point(lexer.KEYWORD, '*do', '*enddo')
-lex:add_fold_point(lexer.KEYWORD, '*DO', '*ENDDO')
 lex:add_fold_point(lexer.KEYWORD, '*dowhile', '*enddo')
-lex:add_fold_point(lexer.KEYWORD, '*DOWHILE', '*ENDDO')
 lex:add_fold_point(lexer.COMMENT, '!', lexer.fold_line_comments('!'))
 
 return lex
--- lexers/batch.lua
+++ lexers/batch.lua
@@ -5,7 +5,7 @@ local lexer = require('lexer')
 local token, word_match = lexer.token, lexer.word_match
 local P, R, S = lpeg.P, lpeg.R, lpeg.S
 
-local lex = lexer.new('batch')
+local lex = lexer.new('batch', {case_insensitive_fold_points = true})
 
 -- Whitespace.
 lex:add_rule('whitespace', token(lexer.WHITESPACE, lexer.space^1))
@@ -48,6 +48,5 @@ lex:add_rule('operator', token(lexer.OPERATOR, S('+|&!<>=')))
 
 -- Fold points.
 lex:add_fold_point(lexer.KEYWORD, 'setlocal', 'endlocal')
-lex:add_fold_point(lexer.KEYWORD, 'SETLOCAL', 'ENDLOCAL')
 
 return lex
