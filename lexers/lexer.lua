1c1f837eae24420aca5db9a78d99ce7ec5d9a9e2
mitchell <70453897+orbitalquark@users.noreply.github.com>
2020-07-14 10:09:33 -0400

    Minor pattern refactoring.

--- lexers/lexer.lua
+++ lexers/lexer.lua
@@ -72,7 +72,7 @@ local M = {}
 --
 --     local lexer = require('lexer')
 --     local token, word_match = lexer.token, lexer.word_match
---     local P, R, S = lpeg.P, lpeg.R, lpeg.S
+--     local P, S = lpeg.P, lpeg.S
 --
 --     local lex = lexer.new('?')
 --
@@ -678,7 +678,7 @@ local M = {}
 --
 --     local lexer = require('lexer')
 --     local token, word_match = lexer.token, lexer.word_match
---     local P, R, S = lpeg.P, lpeg.R, lpeg.S
+--     local P, S = lpeg.P, lpeg.S
 --
 --     local lex = lexer.new('legacy')
 --
