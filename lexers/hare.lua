adeaeddd35e7b859b33c3b03400d00706bbe6b7d
qiu-x <alex@alexslomka.xyz>
2022-06-23 00:01:03 +0200

    Update Hare lexer

--- lexers/hare.lua
+++ lexers/hare.lua
@@ -3,83 +3,64 @@
 -- https://harelang.org
 -- Contributed by Qiu
 
-local l = require('lexer')
-local token, word_match = l.token, l.word_match
+local lexer = require('lexer')
+local token, word_match = lexer.token, lexer.word_match
 local P, R, S = lpeg.P, lpeg.R, lpeg.S
 
-local M = {_NAME = 'hare'}
+local lex = lexer.new('hare')
 
 -- Whitespace.
-local ws = token(l.WHITESPACE, l.space^1)
+lex:add_rule('whitespace', token(lexer.WHITESPACE, lexer.space^1))
 
 -- Comments.
-local line_comment = '//' * l.nonnewline_esc^0
-local comment = token(l.COMMENT, line_comment)
+local line_comment = '//' * lexer.nonnewline_esc^0
+lex:add_rule('comment', token(lexer.COMMENT, line_comment))
 
 -- Strings.
-local dq_str = l.delimited_range('"')
-local raw_str = l.delimited_range('`')
-local string = token(l.STRING, dq_str + raw_str)
+local dq_str = lexer.range('"')
+local raw_str = lexer.range('`')
+lex:add_rule('string', token(lexer.STRING, dq_str + raw_str))
 
 -- Numbers.
-local number = token(l.NUMBER, l.float + l.integer)
+lex:add_rule('number', token(lexer.NUMBER, lexer.float + lexer.integer))
 
 -- Functions.
-local func = token(l.FUNCTION, word_match{
-  'len', 'alloc', 'free', 'assert',
-  'abort', 'size', 'append', 'insert', 'delete',
-  'vastart', 'vaarg', 'vaend'
-})
+lex:add_rule('keyword', token(lexer.FUNCTION, word_match(
+  'len', 'alloc', 'free', 'assert', 'abort', 
+  'size', 'append', 'insert', 'delete', 'vastart', 
+  'vaarg', 'vaend'
+)))
 
 -- Keywords.
-local keyword = token(l.KEYWORD, word_match{
+lex:add_rule('keyword', token(lexer.KEYWORD, word_match(
   'as', 'break', 'case', 'const', 'continue',
   'def', 'defer', 'else', 'export', 'false', 'fn',
   'for', 'if', 'is', 'let', 'match', 'null',
   'nullable', 'return', 'static', 'struct',
   'switch', 'true', 'type',  'use', 'yield'
-})
+)))
 
 -- Types.
-local type = token(l.TYPE, word_match{
+lex:add_rule('type', token(lexer.TYPE, word_match(
   'bool', 'enum', 'f32', 'f64', 'i16', 'i32',
   'i64', 'i8', 'int', 'u16', 'u32', 'u64', 'u8',
   'uint', 'uintptr', 'union', 'void', 'rune',
   'str', 'char'
-})
+)))
 
 -- at rule.
-local at_rule = token('at_rule', P('@') * word_match{
+lex:add_rule('at_rule', token('at_rule', '@' * word_match(
   'noreturn', 'offset', 'init', 'fini', 'test', 'symbol'
-})
+)))
+lex:add_style('at_rule', lexer.styles.preprocessor)
 
 -- Identifiers.
-local identifier = token(l.IDENTIFIER, l.word)
+lex:add_rule('identifier', token(lexer.IDENTIFIER, lexer.word))
 
 -- Operators.
-local operator = token(l.OPERATOR, S('+-/*%^!=&|?:;,.()[]{}<>'))
+lex:add_rule('operator', token(lexer.OPERATOR, S('+-/*%^!=&|?:;,.()[]{}<>')))
 
-M._rules = {
-  {'whitespace', ws},
-  {'keyword', keyword},
-  {'function', func},
-  {'type', type},
-  {'identifier', identifier},
-  {'comment', comment},
-  {'number', number},
-  {'string', string},
-  {'operator', operator},
-  {'at_rule', at_rule},
-}
+lex:add_fold_point(lexer.OPERATOR, '{', '}')
+lex:add_fold_point(lexer.COMMENT, '//')
 
-M._tokenstyles = {
-  at_rule = l.STYLE_PREPROCESSOR
-}
-
-M._foldsymbols = {
-  _patterns = {'[{}]', '//'},
-  [l.OPERATOR] = {['{'] = 1, ['}'] = -1},
-  [l.COMMENT] = {['//'] = l.fold_line_comments('//')}
-}
-
-return M
+return lex
