322c52a7d826f0aa7af6e3a1b287d07704a836be
Matěj Cepl <mcepl@cepl.eu>
2022-07-14 19:04:17 +0200

    Update rpmspec lexer, to make it more useable.
    Unfortunately, this is not working, and I would like to ask for help why.
    
    All these lines should be marked as keywords, but they are not:
    
    Source: source.tar.gz
    Source0: anothersource.tar.gz
    Patch:  first.patch
    Patch0: second.patch

--- lexers/rpmspec.lua
+++ lexers/rpmspec.lua
@@ -2,7 +2,7 @@
 
 local lexer = require('lexer')
 local token, word_match = lexer.token, lexer.word_match
-local P, S = lpeg.P, lpeg.S
+local C, P, R, S = lpeg.C, lpeg.P, lpeg.R, lpeg.S
 
 local lex = lexer.new('rpmspec')
 
@@ -17,16 +17,24 @@ lex:add_rule('string', token(lexer.STRING, lexer.range('"')))
 
 -- Keywords.
 lex:add_rule('keyword', token(lexer.KEYWORD, word_match{
-  'Prereq', 'Summary', 'Name', 'Version', 'Packager', 'Requires', 'Recommends', 'Suggests',
-  'Supplements', 'Enhances', 'Icon', 'URL', 'Source', 'Patch', 'Prefix', 'Packager', 'Group',
-  'License', 'Release', 'BuildRoot', 'Distribution', 'Vendor', 'Provides', 'ExclusiveArch',
-  'ExcludeArch', 'ExclusiveOS', 'Obsoletes', 'BuildArch', 'BuildArchitectures', 'BuildRequires',
-  'BuildConflicts', 'BuildPreReq', 'Conflicts', 'AutoRequires', 'AutoReq', 'AutoReqProv',
-  'AutoProv', 'Epoch'
-}))
+    'Prereq', 'Summary', 'Name', 'Version', 'Packager',
+    'Requires', 'Recommends', 'Suggests', 'Supplements',
+    'Enhances', 'Icon', 'URL', 'Prefix', 'Packager', 'Group',
+    'License', 'Release', 'BuildRoot', 'Distribution', 'Vendor',
+    'Provides', 'ExclusiveArch', 'ExcludeArch', 'ExclusiveOS',
+    'Obsoletes', 'BuildArch', 'BuildArchitectures', 'BuildRequires',
+    'BuildConflicts', 'BuildPreReq', 'Conflicts', 'AutoRequires',
+    'AutoReq', 'AutoReqProv', 'AutoProv', 'Epoch'
+}) + (P('Patch') + P('Source')) * R('09')^0)
 
 -- Macros
-lex:add_rule('command', token(lexer.FUNCTION, '%' * lexer.word))
+lex:add_rule('command', token(lexer.FUNCTION, '%' * lexer.word +
+                                              '%{' * lexer.word * '}'))
+
+-- Constants
+lex:add_rule('constant', token(lexer.CONSTANT, word_match({
+    'rhel', 'fedora', 'suse_version', 'sle_version'
+})))
 
 lexer.property['scintillua.comment'] = '#'
 
