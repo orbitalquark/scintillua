98f09de5aa4f5d1e45d71e55189382d6a53919ea
mitchell <70453897+orbitalquark@users.noreply.github.com>
2022-08-30 12:04:28 -0400

    Updated RHTML lexer.

--- lexers/rhtml.lua
+++ lexers/rhtml.lua
@@ -1,19 +1,18 @@
 -- Copyright 2006-2022 Mitchell. See LICENSE.
 -- RHTML LPeg lexer.
 
-local lexer = require('lexer')
-local word_match = lexer.word_match
+local lexer = lexer
 local P, S = lpeg.P, lpeg.S
 
-local lex = lexer.new('rhtml', {inherit = lexer.load('html')})
+local lex = lexer.new(..., {inherit = lexer.load('html')})
 
 -- Embedded Ruby.
 local ruby = lexer.load('rails')
-local ruby_start_rule = lex:tag('rhtml_tag', '<%' * P('=')^-1)
-local ruby_end_rule = lex:tag('rhtml_tag', '%>')
+local ruby_start_rule = lex:tag(lexer.TAG .. '.rhtml', '<%' * P('=')^-1)
+local ruby_end_rule = lex:tag(lexer.TAG .. '.rhtml', '%>')
 lex:embed(ruby, ruby_start_rule, ruby_end_rule)
 
 -- Fold points.
-lex:add_fold_point('rhtml_tag', '<%', '%>')
+lex:add_fold_point(lexer.TAG .. '.rhtml', '<%', '%>')
 
 return lex
