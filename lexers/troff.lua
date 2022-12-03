d299a959e76c2758bf6c4d41a12d200ee2052b30
Eolien55 <Elie Le Vaillant>
2022-12-03 13:33:42 +0100

    troff: updated legacy lexer and fixed typo

--- lexers/troff.lua
+++ lexers/troff.lua
@@ -1,33 +1,30 @@
 -- Copyright 2015-2017 David B. Lamkins <david@lamkins.net>. See LICENSE.
 -- man/roff LPeg lexer.
 
-local lexer = require('lexer')
-local token, word_match = l.token, l.word_match
+local lexer = lexer
 local P, R, S = lpeg.P, lpeg.R, lpeg.S
 
-local lex = lexer.new('troff')
+local lex = lexer.new(...)
 
--- Whitespace.
-local whitespace = token(l.WHITESPACE, l.space^1)
+-- Registers and groff's structured programming.
+lex:add_rule('keywords', lex:tag(lexer.KEYWORD, (lexer.starts_line('.') * lexer.space^0 * (P('while') + P('break') + P('continue') + P('nr') + P('rr') + P('rnn') + P('aln') + P('\\}'))) + P('\\{'))
 
 -- Markup.
-lex:add_rule('keywords', token(l.KEYWORD, (l.starts_line('.') * l.space^0 * (P('while') + P('break') + P('continue') + P('nr') + P('rr') + P('rnn') + P('aln') + P('\\}'))) + P('\\{'))
-
-lex:add_rule('escape_sequences', token(l.VARIABLE,
+lex:add_rule('escape_sequences', lex:tag(lexer.VARIABLE,
 	P('\\') * ((P('s') * S('+-')) + S('*fgmnYV') + P("")) *
-	(P('(') * 2 + P('[') * (l.any-P(']'))^0 * P(']') + 1))
+	(P('(') * 2 + P('[') * (lexer.any-P(']'))^0 * P(']') + 1))
 
-lex:add_rule('headings', token(l.NUMBER, P('.') * (S('STN') * P('H')) * (l.space + P("\n")) * l.nonnewline^0)
+lex:add_rule('headings', lex:tag(lexer.NUMBER, P('.') * (S('STN') * P('H')) * (lexer.space + P("\n")) * lexer.nonnewline^0)
+lex:add_rule('man_alignment', lex:tag(lexer.KEYWORD, lexer.starts_line('.'), (P('br') + P('DS') + P('RS') + P('RE') + P('PD')) * (lexer.space + P("\n")))
+lex:add_rule('font', lex:tag(lexer.VARIABLE, (P('.B') * P('R')^-1 + P('.I') * S('PR')^-1 + P('.PP')) * (lexer.space + P("\n")))
 
-lex:add_rule('alignment', token(l.KEYWORD, l.starts_line('.'), (P('br') + P('DS') + P('RS') + P('RE') + P('PD')) * (l.space + P("\n")))
+-- Lowercase troff macros are plain macros (like .so or .nr).
+lex:add_rule('troff_plain_macros', lex:tag(lexer.VARIABLE, lexer.starts_line('.') * lexer.space^0 * lexer.lower^1)
+lex:add_rule('any_macro', lex:tag(lexer.PREPROCESSOR, lexer.starts_line('.') * lexer.space^0 * (lexer.any-lexer.space)^0)
+lex:add_rule('comment', lex:tag(lexer.COMMENT, (lexer.starts_line('.\\"') + P('\\"') + P('\\#')) * lexer.nonnewline^0)
+lex:add_rule('string', lex:tag(lexer.STRING, P('"') * (lexer.nonnewline-P('"'))^0 * (P('"') + (P("\n"))))
 
-lex:add_rule('font', token(l.VARIABLE, (P('.B') * P('R')^-1 + P('.I') * S('PR')^-1 + P('.PP')) * (l.space + P("\n")))
-
-lex:add_rule('troff_plain_macros', token(l.VARIABLE, l.starts_line('.') * l.space^0 * l.lower^1)
-lex:add_rule('any_macro', token(l.PREPROCESSOR, l.starts_line('.') * l.space^0 * (l.any-l.space)^0)
-lex:add_rule('comment', token(l.COMMENT, (l.starts_line('.\\"') + P('\\"') + P('\\#')) * l.nonnewline^0)
-lex:add_rule('quoted', token(l.STRING, P('"') * (l.nonnewline-P('"'))^0 * (P('"') + (P("\n"))))
 -- Usually used by eqn, and mandoc in some way.
-lex:add_rule('in_dollars', token(l.EMBEDDED, P('$') * (l.any - P('$'))^0 * P('$'))
+lex:add_rule('in_dollars', lex:tag(lexer.EMBEDDED, P('$') * (lexer.any - P('$'))^0 * P('$'))
 
-return M
+return lex
