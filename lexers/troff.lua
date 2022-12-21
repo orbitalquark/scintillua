0bb6d11a209aea7555e69517f977da629fc94513
Elie Le Vaillant <eolien55@disroot.org>
2022-12-21 22:35:12 +0100

    troff.lua: fixed a ton of errors, mis-designs and typos

--- lexers/troff.lua
+++ lexers/troff.lua
@@ -7,24 +7,24 @@ local P, R, S = lpeg.P, lpeg.R, lpeg.S
 local lex = lexer.new(...)
 
 -- Registers and groff's structured programming.
-lex:add_rule('keywords', lex:tag(lexer.KEYWORD, (lexer.starts_line('.') * lexer.space^0 * (P('while') + P('break') + P('continue') + P('nr') + P('rr') + P('rnn') + P('aln') + P('\\}'))) + P('\\{'))
+lex:add_rule('keywords', lex:tag(lexer.KEYWORD, (lexer.starts_line('.') * lexer.space^0 * (P('while') + P('break') + P('continue') + P('nr') + P('rr') + P('rnn') + P('aln') + P('\\}'))) + P('\\{')))
 
 -- Markup.
 lex:add_rule('escape_sequences', lex:tag(lexer.VARIABLE,
-	P('\\') * ((P('s') * S('+-')) + S('*fgmnYV') + P("")) *
-	(P('(') * 2 + P('[') * (lexer.any-P(']'))^0 * P(']') + 1))
+	P('\\') * ((P('s') * S('+-')) + S('*fgmnYV')) *
+	(P('(') * 2 + lexer.range('[', ']')))
 
-lex:add_rule('headings', lex:tag(lexer.NUMBER, P('.') * (S('STN') * P('H')) * (lexer.space + P("\n")) * lexer.nonnewline^0)
-lex:add_rule('man_alignment', lex:tag(lexer.KEYWORD, lexer.starts_line('.'), (P('br') + P('DS') + P('RS') + P('RE') + P('PD')) * (lexer.space + P("\n")))
-lex:add_rule('font', lex:tag(lexer.VARIABLE, (P('.B') * P('R')^-1 + P('.I') * S('PR')^-1 + P('.PP')) * (lexer.space + P("\n")))
+lex:add_rule('headings', lex:tag(lexer.NUMBER, lexer.starts_line('.') * lexer.space^0 * (S('STN') * P('H')) * lexer.space * lexer.nonnewline^0)
+lex:add_rule('man_alignment', lex:tag(lexer.KEYWORD, lexer.starts_line('.') * lexer.space^0 * (P('br') + P('DS') + P('RS') + P('RE') + P('PD') + P('PP')) * lexer.space))
+lex:add_rule('font', lex:tag(lexer.VARIABLE, lexer.starts_line('.') * lexer.space^0 * (P('B') * P('R')^-1 + P('I') * S('PR')^-1) * lexer.space))
 
 -- Lowercase troff macros are plain macros (like .so or .nr).
 lex:add_rule('troff_plain_macros', lex:tag(lexer.VARIABLE, lexer.starts_line('.') * lexer.space^0 * lexer.lower^1)
 lex:add_rule('any_macro', lex:tag(lexer.PREPROCESSOR, lexer.starts_line('.') * lexer.space^0 * (lexer.any-lexer.space)^0)
 lex:add_rule('comment', lex:tag(lexer.COMMENT, (lexer.starts_line('.\\"') + P('\\"') + P('\\#')) * lexer.nonnewline^0)
-lex:add_rule('string', lex:tag(lexer.STRING, P('"') * (lexer.nonnewline-P('"'))^0 * (P('"') + (P("\n"))))
+lex:add_rule('string', lex:tag(lexer.STRING, lexer.range('"', true))
 
 -- Usually used by eqn, and mandoc in some way.
-lex:add_rule('in_dollars', lex:tag(lexer.EMBEDDED, P('$') * (lexer.any - P('$'))^0 * P('$'))
+lex:add_rule('in_dollars', lex:tag(lexer.EMBEDDED, lexer.range('$', false, false))
 
 return lex
