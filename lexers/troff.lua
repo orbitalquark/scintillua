a135d2e26593884b16e3523402ad4c45b7909393
Elie Le Vaillant <eolien55@disroot.org>
2022-12-22 13:13:10 +0100

    troff.lua: l.space -> l.space-'\n'

--- lexers/troff.lua
+++ lexers/troff.lua
@@ -7,20 +7,20 @@ local P, R, S = lpeg.P, lpeg.R, lpeg.S
 local lex = lexer.new(...)
 
 -- Registers and groff's structured programming.
-lex:add_rule('keywords', lex:tag(lexer.KEYWORD, (lexer.starts_line('.') * lexer.space^0 * (P('while') + P('break') + P('continue') + P('nr') + P('rr') + P('rnn') + P('aln') + P('\\}'))) + P('\\{')))
+lex:add_rule('keywords', lex:tag(lexer.KEYWORD, (lexer.starts_line('.') * (lexer.space-'\n')^0 * (P('while') + P('break') + P('continue') + P('nr') + P('rr') + P('rnn') + P('aln') + P('\\}'))) + P('\\{')))
 
 -- Markup.
 lex:add_rule('escape_sequences', lex:tag(lexer.VARIABLE,
-	P('\\') * ((P('s') * S('+-')) + S('*fgmnYV')) *
+	P('\\') * ((P('s') * S('+-')) + S('*fgmnYV'))^-1 *
 	(P('(') * 2 + lexer.range('[', ']') + 1))
 
-lex:add_rule('headings', lex:tag(lexer.NUMBER, lexer.starts_line('.') * lexer.space^0 * (S('STN') * P('H')) * lexer.space * lexer.nonnewline^0)
-lex:add_rule('man_alignment', lex:tag(lexer.KEYWORD, lexer.starts_line('.') * lexer.space^0 * (P('br') + P('DS') + P('RS') + P('RE') + P('PD') + P('PP')) * lexer.space))
-lex:add_rule('font', lex:tag(lexer.VARIABLE, lexer.starts_line('.') * lexer.space^0 * (P('B') * P('R')^-1 + P('I') * S('PR')^-1) * lexer.space))
+lex:add_rule('headings', lex:tag(lexer.NUMBER, lexer.starts_line('.') * (lexer.space-'\n')^0 * (S('STN') * P('H')) * (lexer.space-'\n') * lexer.nonnewline^0)
+lex:add_rule('man_alignment', lex:tag(lexer.KEYWORD, lexer.starts_line('.') * (lexer.space-'\n')^0 * (P('br') + P('DS') + P('RS') + P('RE') + P('PD') + P('PP')) * lexer.space))
+lex:add_rule('font', lex:tag(lexer.VARIABLE, lexer.starts_line('.') * (lexer.space-'\n')^0 * (P('B') * P('R')^-1 + P('I') * S('PR')^-1) * lexer.space))
 
 -- Lowercase troff macros are plain macros (like .so or .nr).
-lex:add_rule('troff_plain_macros', lex:tag(lexer.VARIABLE, lexer.starts_line('.') * lexer.space^0 * lexer.lower^1)
-lex:add_rule('any_macro', lex:tag(lexer.PREPROCESSOR, lexer.starts_line('.') * lexer.space^0 * (lexer.any-lexer.space)^0)
+lex:add_rule('troff_plain_macros', lex:tag(lexer.VARIABLE, lexer.starts_line('.') * (lexer.space-'\n')^0 * lexer.lower^1)
+lex:add_rule('any_macro', lex:tag(lexer.PREPROCESSOR, lexer.starts_line('.') * (lexer.space-'\n')^0 * (lexer.any-lexer.space)^0)
 lex:add_rule('comment', lex:tag(lexer.COMMENT, (lexer.starts_line('.\\"') + P('\\"') + P('\\#')) * lexer.nonnewline^0)
 lex:add_rule('string', lex:tag(lexer.STRING, lexer.range('"', true))
 
