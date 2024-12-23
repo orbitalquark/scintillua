179dcd03f5cbd1dc1febf5d8a3c1e7ab01efc987
Samuel Marquis <smarquis@protonmail.com>
2024-12-22 21:00:47 -0500

    Grab builtins object, other refactor

--- lexers/nix.lua
+++ lexers/nix.lua
@@ -6,14 +6,18 @@ local P, S = lpeg.P, lpeg.S
 
 local lex = lexer.new(...)
 
+local id_char = lexer.alnum + P("_'")
+
 -- Keywords.
 lex:add_rule('keyword', lex:tag(lexer.KEYWORD, lex:word_match(lexer.KEYWORD)))
 
 -- Functions.
-lex:add_rule('function', lex:tag(lexer.FUNCTION_BUILTIN, lex:word_match(lexer.FUNCTION_BUILTIN)))
+lex:add_rule('function', lex:tag(lexer.FUNCTION,
+	(P('builtin.')^0 * lex:word_match(lexer.FUNCTION)) + lex:word_match(lexer.FUNCTION_BUILTIN)))
 
 -- Constants.
-lex:add_rule('constant', lex:tag(lexer.CONSTANT_BUILTIN, lex:word_match(lexer.CONSTANT_BUILTIN)))
+lex:add_rule('constant', lex:tag(lexer.CONSTANT,
+	(P('builtin.')^0 * lex:word_match(lexer.CONSTANT)) + lex:word_match(lexer.CONSTANT_BUILTIN)))
 
 -- Strings.
 local str = lexer.range('"', true)
@@ -23,7 +27,7 @@ lex:add_rule('string', lex:tag(lexer.STRING, str + ml_str))
 -- Paths.
 local path_char = lexer.alnum + S('_-.+')
 local path_seg = ('/' * path_char^1)
-local path = P('~')^-1 * path_char^0 * path_seg^1 * lpeg.P('/')^-1
+local path = P('~')^-1 * path_char^0 * path_seg^1 * P('/')^-1
 lex:add_rule('path', lex:tag(lexer.LINK, path))
 
 -- URIs.
@@ -35,9 +39,21 @@ lex:add_rule('uri', lex:tag(lexer.LINK, uri))
 local spath = '<' * path_char^1 * path_seg^0 * '>'
 lex:add_rule('spath', lex:tag(lexer.LINK, spath))
 
+-- Packages.
+-- As it stands, this is incorrect. It would be NICE to have every identifier inside
+-- a package list tagged the same way, but you can put normal Nix expressions in there
+-- too, which would have to get parsed somehow--and you probably don't want to eat the
+-- brackets here. I don't know how to filter down ranges off the top of my head,
+-- so I'll leave this one to you if you feel like workshopping it.
+local pkg_list = lexer.range('[', ']')
+lex:add_rule('pkgs', lex:tag(lexer.VARIABLE, pkg_list))
+
 -- Identifiers.
-local id = (lexer.alpha + '_') * (lexer.alnum + S('_-'))^0
-lex:add_rule('identifier', lex:tag(lexer.IDENTIFIER, id))
+-- This feels dubious. There's no good source on what is and isn't a valid identifier
+-- as far as I can tell. Packages can use _-, functions can use _', and there's no hex
+-- or octal, or literal suffixes, so... maybe identifiers can start with numbers?
+-- I hope not.
+lex:add_rule('identifier', lex:tag(lexer.IDENTIFIER, id_char^1))
 
 -- Comments.
 local line_comment = lexer.to_eol('#', true)
@@ -64,9 +80,15 @@ lex:set_word_list(lexer.KEYWORD, {
 	'let', 'in', 'rec', 'inherit', 'or', '...',
 })
 
+-- Perhaps paradoxically, the BUILTIN variants are the ones that don't require
+-- the `builtins` object. I don't know, it seems like the right approach? The BUILTINS
+-- are, logically, the ones you don't need to do anything to get at... and yet it feels so gone ...
 lex:set_word_list(lexer.CONSTANT_BUILTIN, {
 	-- Directly accessible.
 	'builtins', 'true', 'false', 'null',
+})
+
+lex:set_word_list(lexer.CONSTANT, {
 	-- Accessible via the global `builtins` object.
 	'currentSystem', 'currentTime', 'langVersion',
 	'nixPath', 'nixVersion', 'storeDir',
@@ -75,6 +97,9 @@ lex:set_word_list(lexer.CONSTANT_BUILTIN, {
 lex:set_word_list(lexer.FUNCTION_BUILTIN, {
 	-- Directly accessible.
 	'derivation', 'import', 'abort', 'throw',
+})
+
+lex:set_word_list(lexer.FUNCTION, {
 	-- Accessible via the global `builtins` object.
 	'add', 'all', 'any', 'attrNames', 'attrValues', 'baseNameOf', 'bitAnd',
 	'bitOr', 'bitXor', 'break', 'catAttrs', 'ceil', 'compareVersions',
