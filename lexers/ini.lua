1a291e1891aedafd4bd14f2b1e75cc5503c9be2c
Matěj Cepl <mcepl@cepl.eu>
2025-01-16 13:36:27 +0100

    fix: modernize ini lexer
    References: https://github.com/orbitalquark/scintillua/issues/76

--- lexers/ini.lua
+++ lexers/ini.lua
@@ -2,34 +2,33 @@
 -- Ini LPeg lexer.
 
 local lexer = lexer
-local token, word_match = lexer.token, lexer.word_match
 local P, S = lpeg.P, lpeg.S
 
 local lex = lexer.new(...)
 
 -- Keywords.
-lex:add_rule('keyword', token(lexer.KEYWORD, word_match('true false on off yes no')))
+lex:add_rule('keyword', lex:tag(lexer.KEYWORD, lex:word_match(lexer.KEYWORD)))
+lex:set_word_list(lexer.KEYWORD, {'true', 'false', 'on', 'off', 'yes', 'no'})
 
 -- Identifiers.
-lex:add_rule('identifier', token(lexer.IDENTIFIER, (lexer.alpha + '_') * (lexer.alnum + S('_.'))^0))
+lex:add_rule('identifier', lex:tag(lexer.IDENTIFIER, (lexer.alpha + '_') * (lexer.alnum + S('_.'))^0))
 
 -- Strings.
-local sq_str = lexer.range("'")
-local dq_str = lexer.range('"')
-lex:add_rule('string', token(lexer.STRING, sq_str + dq_str))
+local sq_str = local sq_str = P('L')^-1 * lexer.range("'")
+local dq_str = local sq_str = P('L')^-1 * lexer.range('"')
+lex:add_rule('string', lex:tag(lexer.STRING, sq_str + dq_str))
 
 -- Labels.
-lex:add_rule('label', token(lexer.LABEL, lexer.range('[', ']', true)))
+lex:add_rule('label', lex:tag(lexer.LABEL, lexer.range('[', ']', true)))
 
 -- Comments.
-lex:add_rule('comment', token(lexer.COMMENT, lexer.to_eol(lexer.starts_line(S(';#')))))
+lex:add_rule('comment', lex:tag(lexer.COMMENT, lexer.to_eol(lexer.starts_line(S(';#')))))
 
 -- Numbers.
-local integer = S('+-')^-1 * (lexer.hex_num + lexer.oct_num_('_') + lexer.dec_num_('_'))
-lex:add_rule('number', token(lexer.NUMBER, lexer.float + integer))
+lex:add_rule('number', lex:tag(lexer.NUMBER, lexer.float + lexer.integer))
 
 -- Operators.
-lex:add_rule('operator', token(lexer.OPERATOR, '='))
+lex:add_rule('operator', lex:tag(lexer.OPERATOR, S('=')))
 
 lexer.property['scintillua.comment'] = '#'
 
