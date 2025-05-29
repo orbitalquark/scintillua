04c725745d85ca7e25541f305e6318be8fb673a5
Matěj Cepl <mcepl@cepl.eu>
2025-05-29 08:46:29 +0200

    fix: correct the 'tag' rule to be more explicit about the ending

--- lexers/mediawiki.lua
+++ lexers/mediawiki.lua
@@ -1,25 +1,20 @@
 -- Copyright 2006-2025 Mitchell. See LICENSE.
 -- MediaWiki LPeg lexer.
 -- Contributed by Alexander Misel.
-
 local lexer = lexer
 local P, S, B = lpeg.P, lpeg.S, lpeg.B
-
 local lex = lexer.new(...)
 
 -- HTML-like tags
 local dq_str = P('"') * ((lexer.any - S('>"\\')) + ('\\' * lexer.any))^0 * P('"')
--- Unquoted attributes (can't contain spaces, quotes, or angle brackets)
 local unquoted_attr = (lexer.any - (S('"' .. "'" .. '<>=') + lexer.space))^1
 local tag_attr = lex:tag(lexer.ATTRIBUTE, lexer.alpha^1 * lexer.space^0 *
-    ('=' * lexer.space^0 * (dq_str + unquoted_attr))^-1 * lexer.space^0)
-local tag_start = lex:tag(lexer.TAG, '<' * P('/')^-1 * lexer.alnum^1 * lexer.space^0)
-local tag_end = lex:tag(lexer.TAG, P('/')^-1 * '>')
--- The tag rule captures the start tag, attributes, and then optionally a closing tag.
--- A more robust solution might distinguish self-closing tags
--- (<br />) from paired tags (<div>...</div>)
-lex:add_rule('tag', tag_start * tag_attr^0 * (P('/')^-1 * tag_end)^-1)
+	('=' * lexer.space^0 * (dq_str + unquoted_attr))^-1 * lexer.space^0)
+local tag_name = lexer.alnum^1
+local tag_inner_content = lexer.space^0 * tag_attr^0 * lexer.space^0 * P('/')^-1
 
+-- The tag rule should consume from '<' to the final '>'
+lex:add_rule('tag', lex:tag(lexer.TAG, '<' * P('/')^-1 * tag_name * tag_inner_content * P('>')))
 
 -- Internal Link: [[Target]] or [[Target|Display Text]]
 -- The content can contain almost anything except unbalanced square brackets.
@@ -33,24 +28,21 @@ local protocol = lexer.alpha^2 * P('://')
 local external_link_content = (protocol * (lexer.any - P(']'))^1) + (lexer.any - P(']'))^1
 lex:add_rule('external_link', lex:tag(lexer.LINK, P('[') * external_link_content * P(']')))
 
-
 -- Parser Functions: {{#function:args}} or {{function:args}}
 -- This is a very complex area. This lexer assumes a simple "name:" pattern.
 -- Tag the function name and its arguments.
 local parser_function_name = P('#')^-1 * (lexer.alpha + S('_'))^1 * P(':')
-local parser_function_content = (lexer.any - S('{}'))^0
+local parser_function_content = (lexer.any - S('{}'))^1
 lex:add_rule('parser_func',
 	lex:tag(lexer.FUNCTION, P('{{') * parser_function_name * parser_function_content * P('}}')))
-
 
 -- Templates and Variables: {{TemplateName|args}} or {{VARIABLENAME}}
 -- Tag the template/variable name.
 -- This rule needs to be placed *after* parser_func if there's any ambiguity in parsing.
 local template_or_variable_name = (lexer.alnum + S('_'))^1
-local template_content = (lexer.any - S('{}'))^0 -- Content up to closing braces
+local template_content = (P('|') * (lexer.any - S('{}'))^1)^-1 -- Optional content after |
 lex:add_rule('template',
 	lex:tag(lexer.VARIABLE, P('{{') * template_or_variable_name * template_content * P('}}')))
-
 
 -- Headings (e.g., == My Heading ==)
 -- Capture the heading text as lexer.HEADING
@@ -59,19 +51,17 @@ lex:add_rule('heading',
 	lex:tag(lexer.HEADING, lexer.starts_line(heading_level * lexer.space^0 *
 		(lexer.any - S('=') - lexer.newline)^1 * lexer.space^0 * heading_level)))
 
-
 -- Operators.
 -- Consider adding more specific rules for bold/italic instead of general operators.
 -- For now, keep existing general operators.
 lex:add_rule('operator', lex:tag(lexer.OPERATOR, S('-=|#~!')))
 
 -- Behavior switches (e.g., __TOC__)
-local start_pat = P(function(_, pos) return pos == 1 end)
 lex:add_rule('behavior_switch',
-    ((B(lexer.space) + start_pat) * lex:word_match('behavior_switch') * #lexer.space))
+    lex:tag(lexer.KEYWORD, lex:word_match('behavior_switch')))
 
 -- Comments.
-lex:add_rule('comment', lex:tag(lexer.COMMENT, lexer.range('')))
+lex:add_rule('comment', lex:tag(lexer.COMMENT, lexer.range('<!--', '-->')))
 
 -- Word lists
 lex:set_word_list('behavior_switch',
