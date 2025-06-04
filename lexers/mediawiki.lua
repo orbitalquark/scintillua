511b01493f9c8a63e9356052bfe0fb3fc09559ca
Matěj Cepl <mcepl@cepl.eu>
2025-06-04 19:17:16 +0200

    fix(mediawiki): accommodate reviewers requests
    * fix `tag` rule to allow using attributes properly
    * replace homegrown definitions with the ones from lexer.*
    * separate keyword lists to separate section

--- lexers/mediawiki.lua
+++ lexers/mediawiki.lua
@@ -1,73 +1,66 @@
 -- Copyright 2006-2025 Mitchell. See LICENSE.
 -- MediaWiki LPeg lexer.
 -- Contributed by Alexander Misel.
+
 local lexer = lexer
-local P, S, B = lpeg.P, lpeg.S, lpeg.B
+local P, S = lpeg.P, lpeg.S
+
 local lex = lexer.new(...)
 
--- HTML-like tags
-local dq_str = P('"') * ((lexer.any - S('>"\\')) + ('\\' * lexer.any))^0 * P('"')
-local unquoted_attr = (lexer.any - (S('"' .. "'" .. '<>=') + lexer.space))^1
-local tag_attr = lex:tag(lexer.ATTRIBUTE, lexer.alpha^1 * lexer.space^0 *
-	('=' * lexer.space^0 * (dq_str + unquoted_attr))^-1 * lexer.space^0)
-local tag_name = lexer.alnum^1
-local tag_inner_content = lexer.space^0 * tag_attr^0 * lexer.space^0 * P('/')^-1
-
--- The tag rule should consume from '<' to the final '>'
-lex:add_rule('tag', lex:tag(lexer.TAG, '<' * P('/')^-1 * tag_name * tag_inner_content * P('>')))
-
--- Internal Link: [[Target]] or [[Target|Display Text]]
--- The content can contain almost anything except unbalanced square brackets.
--- We'll highlight the whole thing as LINK.
-local internal_link_content = (lexer.any - P(']]'))^1 -- Matches everything until ']]'
-lex:add_rule('internal_link', lex:tag(lexer.LINK, P('[[') * internal_link_content * P(']]')))
-
--- External Link: [http://example.com Link text] or [http://example.com]
--- Content should start with a protocol (http/s, ftp, mailto etc.)
-local protocol = lexer.alpha^2 * P('://')
-local external_link_content = (protocol * (lexer.any - P(']'))^1) + (lexer.any - P(']'))^1
-lex:add_rule('external_link', lex:tag(lexer.LINK, P('[') * external_link_content * P(']')))
-
--- Parser Functions: {{#function:args}} or {{function:args}}
--- This is a very complex area. This lexer assumes a simple "name:" pattern.
--- Tag the function name and its arguments.
-local parser_function_name = P('#')^-1 * (lexer.alpha + S('_'))^1 * P(':')
-local parser_function_content = (lexer.any - S('{}'))^1
-lex:add_rule('parser_func',
-	lex:tag(lexer.FUNCTION, P('{{') * parser_function_name * parser_function_content * P('}}')))
-
--- Templates and Variables: {{TemplateName|args}} or {{VARIABLENAME}}
--- Tag the template/variable name.
--- This rule needs to be placed *after* parser_func if there's any ambiguity in parsing.
-local template_or_variable_name = (lexer.alnum + S('_'))^1
-local template_content = (P('|') * (lexer.any - S('{}'))^1)^-1 -- Optional content after |
-lex:add_rule('template',
-	lex:tag(lexer.VARIABLE, P('{{') * template_or_variable_name * template_content * P('}}')))
-
--- Headings (e.g., == My Heading ==)
--- Capture the heading text as lexer.HEADING
-local heading_level = S('=')^1
-lex:add_rule('heading',
-	lex:tag(lexer.HEADING, lexer.starts_line(heading_level * lexer.space^0 *
-		(lexer.any - S('=') - lexer.newline)^1 * lexer.space^0 * heading_level)))
-
--- Operators.
--- Consider adding more specific rules for bold/italic instead of general operators.
--- For now, keep existing general operators.
-lex:add_rule('operator', lex:tag(lexer.OPERATOR, S('-=|#~!')))
-
--- Behavior switches (e.g., __TOC__)
-lex:add_rule('behavior_switch',
-    lex:tag(lexer.KEYWORD, lex:word_match('behavior_switch')))
-
--- Comments.
+-- Comments (high priority to avoid conflicts)
 lex:add_rule('comment', lex:tag(lexer.COMMENT, lexer.range('<!--', '-->')))
 
--- Word lists
-lex:set_word_list('behavior_switch',
-	{'__TOC__', '__FORCETOC__', '__NOTOC__', '__NOEDITSECTION__', '__NOCC__',
-	'__NOINDEX__', '__NOKEYWORDLINK__', '__NOCONTENTCONVERT__', '__NOEDITSECTION__'})
+-- HTML-like tags
+local unquoted_attr = (lexer.any - (S('"' .. "'" .. '<>=') + lexer.space))^1
+local tag_name = lexer.alpha^1
+local tag_attr = lex:tag(lexer.ATTRIBUTE, lexer.alpha^1 * lexer.space^0 *
+	('=' * lexer.space^0 * (lexer.range('"') + unquoted_attr))^-1 * lexer.space^0)
+lex:add_rule('tag', lex:tag(lexer.TAG, '<' * P('/')^-1 * tag_name *
+	(lexer.space^1 * tag_attr)^0 * lexer.space^0 * P('/')^-1 * '>'))
 
+-- Internal Links
+lex:add_rule('internal_link', lex:tag(lexer.LINK, lexer.range('[[', ']]')))
+
+-- External Links
+lex:add_rule('external_link', lex:tag(lexer.LINK,
+	P('[') * lex:word_match(lexer.TYPE) * P('://') *
+	(lexer.any - P(']'))^0 * P(']')))
+
+-- Parser Functions
+lex:add_rule('parser_func', lex:tag(lexer.FUNCTION,
+	P('{{') * P('#')^-1 * lexer.alpha^1 * P(':') *
+	(lexer.any - S('{}'))^0 * P('}}')))
+
+-- Templates and Variables
+lex:add_rule('template', lex:tag(lexer.VARIABLE,
+	P('{{') * lexer.alpha^1 * (P('|') * (lexer.any - S('{}'))^0)^0 * P('}}')))
+
+-- Headings
+lex:add_rule('heading', lex:tag(lexer.HEADING,
+	lexer.starts_line(S('=')^2 * lexer.space^0 *
+	(lexer.any - S('=\r\n'))^1 * lexer.space^0 * S('=')^2)))
+
+-- Bold and Italic formatting
+lex:add_rule('bold', lex:tag(lexer.BOLD, lexer.range("'''", "'''")))
+lex:add_rule('italic', lex:tag(lexer.ITALIC, lexer.range("''", "''")))
+
+-- Behavior switches
+lex:add_rule('behavior_switch',
+	lex:tag(lexer.PREPROCESSOR, lex:word_match(lexer.PREPROCESSOR)))
+
+-- Word lists
+lex:set_word_list(lexer.TYPE, {
+	'http', 'https', 'ftp', 'ftps', 'mailto', 'news', 'irc', 'gopher'
+})
+
+lex:set_word_list(lexer.PREPROCESSOR, {
+	'__NOTOC__', '__FORCETOC__', '__TOC__', '__NOEDITSECTION__', '__NEWSECTIONLINK__',
+	'__NONEWSECTIONLINK__', '__NOGALLERY__', '__HIDDENCAT__', '__NOCONTENTCONVERT__',
+	'__NOCC__', '__NOTITLECONVERT__', '__NOTC__', '__START__', '__END__', '__INDEX__',
+	'__NOINDEX__', '__STATICREDIRECT__', '__DISAMBIG__'
+})
+
+--- Properties
 lexer.property['scintillua.comment'] = '<!--|-->'
 lexer.property['scintillua.angle.braces'] = '1'
 
