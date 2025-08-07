194204aee83d51c4181556c7ea1d259939fa041d
orbitalquark <70453897+orbitalquark@users.noreply.github.com>
2025-08-07 16:36:10 -0400

    Tokenize range operators for the languages that support them.
    This avoids highlighting ranges like 0..1 incorrectly.

--- lexers/pascal.lua
+++ lexers/pascal.lua
@@ -61,7 +61,7 @@ local integer = hex_num + oct_num + bin_num + lexer.integer
 lex:add_rule('number', token(lexer.NUMBER, lexer.float + integer))
 
 -- Operators.
-lex:add_rule('operator', token(lexer.OPERATOR, S('.,;^@:=<>+-/*()[]')))
+lex:add_rule('operator', token(lexer.OPERATOR, '..' + S('.,;^@:=<>+-/*()[]')))
 
 lexer.property['scintillua.comment'] = '//'
 
