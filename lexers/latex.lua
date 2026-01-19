19ff62fd788aac653d424206b8e0398bb14343c9
orbitalquark <70453897+orbitalquark@users.noreply.github.com>
2026-01-19 14:01:37 -0500

    Added more LaTeX math environment keywords.

--- lexers/latex.lua
+++ lexers/latex.lua
@@ -16,7 +16,8 @@ local block_comment = lexer.range('\\begin' * P(' ')^0 * '{comment}',
 lex:add_rule('comment', lex:tag(lexer.COMMENT, line_comment + block_comment))
 
 -- Math environments.
-local math_word = word_match('align displaymath eqnarray equation gather math multline')
+local math_word = word_match(
+	'align alignat displaymath eqnarray equation flalign gather math multline')
 local math_begin_end = (P('begin') + P('end')) * P(' ')^0 * '{' * math_word * P('*')^-1 * '}'
 lex:add_rule('math', lex:tag('environment.math', '$' + '\\' * (S('[]()') + math_begin_end)))
 
