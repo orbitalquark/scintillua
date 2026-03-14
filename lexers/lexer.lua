e0abdbb4a125f4fd4d0ab4f8db5f3203fd06cb5d
Jamie Drinkell <94254558+M0JXD@users.noreply.github.com>
2026-03-14 19:22:27 +0000

    Match formatting

--- lexers/lexer.lua
+++ lexers/lexer.lua
@@ -1749,7 +1749,7 @@ function M.detect(filename, line)
 		htm = 'html', html = 'html', shtm = 'html', shtml = 'html', xhtml = 'html', vue = 'html', --
 		icn = 'icon', --
 		idl = 'idl', odl = 'idl', --
-		ino = 'arduino',
+		ino = 'arduino', --
 		ni = 'inform', --
 		cfg = 'ini', cnf = 'ini', inf = 'ini', ini = 'ini', reg = 'ini', --
 		io = 'io_lang', --
